574a3dc85c25a9538951f046fbd9a6b809938846
Scott Fraser <scottfraserdev@gmail.com>
2022-12-18 20:49:23 -0800

    basic scott.com

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h3>Scott Fraser</h3>
+        <label>Software Engineer</label>
+        <h4>
+          <a href='https://www.linkedin.com/in/scott-a-fraser/'>LinkedIn</a>
+        </h4>
       </header>
     </div>
   );
